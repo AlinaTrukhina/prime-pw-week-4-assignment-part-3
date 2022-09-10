b86edb000e072c1049f9dde1fefc5c64dd8c20ec
Alina Trukhina <alina.trukhina@gmail.com>
2022-09-10 14:07:31 -0500

    started js

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,15 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+// create basket
+const basket = [];
+
+// creat function to add items to basket
+function addItem(item) {
+    basket.push(item);
+    return true;
+}
+
+console.log('test addItem', addItem('cucumber'));
+console.log(basket);
