e6273f12e732c0f2b5074eeb51f6a57ab377a319
Alina Trukhina <alina.trukhina@gmail.com>
2022-09-10 16:21:18 -0500

    created listItems

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -7,14 +7,25 @@ const basket = [];
 
 // create function to add items to basket
 function addItem(item) {
-    item.toString();
+//    item.toString();
         basket.push(item);
         return true;
     } 
 
 //test function
 console.log('test addItem', addItem('cucumber'));
-console.log(bread);
+// console.log(bread); this returns an error saying bread is not defined.
+console.log('test addItem', addItem('tomato'));
+console.log('test addItem', addItem('onion'));
 console.log(basket);
 
 // create function to list items
+function listItems(basket) {
+    for (let n in basket) {
+        console.log(basket[n]);
+    }
+}
+
+// test listItems
+listItems(basket);
+console.log(listItems(basket));
